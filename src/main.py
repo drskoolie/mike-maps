0feb5e0b659f3a82ec9163a61b10bd86bf13c44b
Al-Baraa El-Hag <a.elhag01@gmail.com>
2023-04-26 17:40:13 -0400

    Rename columns setup

--- src/main.py
+++ src/main.py
@@ -2,11 +2,38 @@
 import os
 import pandas as pd
 
-data_directory = 'data/raw/pollbypoll_bureauparbureauCanada/'
+data_directory = "data/raw/pollbypoll_bureauparbureauCanada/"
 polls = os.listdir(data_directory)
-for poll in polls:
-    if poll[:5] != 'table':
-        print(poll)
+
+## Part 1: Column Renaming
 
 df = pd.read_csv(data_directory + polls[0])
-df
+
+columns_original = [
+    "Electoral District Number/Numéro de circonscription",
+    "Electoral District Name/Nom de circonscription",
+    "Polling Station Number/Numéro du bureau de scrutin",
+    "Polling Station Name/Nom du bureau de scrutin",
+    "Rejected Ballots/Bulletins rejetés",
+    "Total Votes/Total des votes",
+    "Electors/Électeurs",
+]
+
+columns_condensed = [
+    "District Number",
+    "District Name",
+    "Polling Number",
+    "Polling Name",
+    "Rejected Ballots",
+    "Total Votes",
+    "Electors",
+]
+
+columns_dict = dict(zip(columns_original, columns_condensed))
+df = df.rename(columns=columns_dict)
+df = df.rename(str.lower, axis='columns')
+
+# Part 2: Iterating
+for poll in polls:
+    if poll[:5] != "table":
+        pass
